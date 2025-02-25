ba4007486bf6dbf1ef0405030d0735d21f7f01eb
Luke Baumann <lukebaumann@google.com>
2025-02-25 15:51:36 -0800

    Introduce `pathwaysutils.initialize()` to remove relying on side-effects from `import pathwaysutils`.
    PiperOrigin-RevId: 731059752

--- pathwaysutils/__init__.py
+++ pathwaysutils/__init__.py
@@ -16,6 +16,7 @@
 import datetime
 import logging
 import os
+import warnings
 
 import jax
 from pathwaysutils import cloud_logging
@@ -24,9 +25,8 @@ from pathwaysutils import proxy_backend
 from pathwaysutils.persistence import orbax_handler
 
 
-logger = logging.getLogger(__name__)
-
-# A new PyPI release will be pushed every time `__version__` is increased.
+_logger = logging.getLogger(__name__)
+_initialization_count = 0
 # When changing this, also update the CHANGELOG.md.
 __version__ = "v0.0.8"
 
@@ -51,26 +51,53 @@ def _is_persistence_enabled():
   return False
 
 
-if _is_pathways_used():
-  logger.debug(
-      "pathwaysutils: Detected Pathways-on-Cloud backend. Applying changes."
-  )
-  proxy_backend.register_backend_factory()
-  profiling.monkey_patch_jax()
-  # TODO(b/365549911): Remove when OCDBT-compatible
-  if _is_persistence_enabled():
-    orbax_handler.register_pathways_handlers(datetime.timedelta(hours=1))
+def initialize():
+  """Initializes pathwaysutils."""
+  global _initialization_count
+  _initialization_count += 1
 
-  # Turn off JAX compilation cache because Pathways handles its own compilation
-  # cache.
-  jax.config.update("jax_enable_compilation_cache", False)
+  if _initialization_count == 1:
+    warnings.warn(
+        "pathwaysutils: Legacy initialization. Ensure you also call"
+        " pathwaysutils.initialize(). This warning will be removed in a future"
+        " release."
+    )
 
-  try:
-    cloud_logging.setup()
-  except OSError as e:
-    logger.debug("pathwaysutils: Failed to set up cloud logging.")
-else:
-  logger.debug(
-      "pathwaysutils: Did not detect Pathways-on-Cloud backend. No changes"
-      " applied."
-  )
+  # Ignoring the second call to initialize() is a temporary measure so that this warning is not triggered for customers who are following our instructions and using the new initialize() function only once but have already had the legacy initialization triggered.
+  if _initialization_count > 2:
+    warnings.warn(
+        "pathwaysutils: Already initialized. Ignoring duplicate call."
+    )
+
+  if _initialization_count > 1:
+    return
+
+  if _is_pathways_used():
+    _logger.debug(
+        "pathwaysutils: Detected Pathways-on-Cloud backend. Applying changes."
+    )
+    proxy_backend.register_backend_factory()
+    profiling.monkey_patch_jax()
+    # TODO: b/365549911 - Remove when OCDBT-compatible
+    if _is_persistence_enabled():
+      orbax_handler.register_pathways_handlers(datetime.timedelta(hours=1))
+
+    # Turn off JAX compilation cache because Pathways handles its own
+    # compilation cache.
+    jax.config.update("jax_enable_compilation_cache", False)
+
+    try:
+      cloud_logging.setup()
+    except Exception as error:  # pylint: disable=broad-except
+      _logger.debug(
+          "pathwaysutils: Failed to set up cloud logging due to the following"
+          " error: %s",
+          error,
+      )
+  else:
+    _logger.debug(
+        "pathwaysutils: Did not detect Pathways-on-Cloud backend. No changes"
+        " applied."
+    )
+
+initialize()
