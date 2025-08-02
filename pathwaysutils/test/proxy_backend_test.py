171f4cde979f42ac133eaa05c92dfb4e4dd98475
Luke Baumann <lukebaumann@google.com>
2025-08-01 17:13:49 -0700

    Introduce jax shim to support multiple versions of JAX
    PiperOrigin-RevId: 789952304

--- pathwaysutils/test/proxy_backend_test.py
+++ pathwaysutils/test/proxy_backend_test.py
@@ -17,7 +17,7 @@ from unittest import mock
 
 import jax
 from jax.extend import backend
-from jax.lib.xla_extension import ifrt_proxy
+from pathwaysutils import jax as pw_jax
 from pathwaysutils import proxy_backend
 
 from absl.testing import absltest
@@ -38,7 +38,7 @@ class ProxyBackendTest(absltest.TestCase):
   def test_proxy_backend_registration(self):
     self.enter_context(
         mock.patch.object(
-            ifrt_proxy,
+            pw_jax.ifrt_proxy,
             "get_client",
             return_value=mock.MagicMock(),
         )
