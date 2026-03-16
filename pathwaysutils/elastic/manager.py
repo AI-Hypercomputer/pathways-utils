a57c2a050a07e08f0ec5085b3e1fe20565b2b80e
Luke Baumann <lukebaumann@google.com>
2026-03-16 11:10:32 -0700

    Add a replica_resize decorator for fault tolerance in elastic Pathways.
    PiperOrigin-RevId: 884542042

--- pathwaysutils/elastic/manager.py
+++ pathwaysutils/elastic/manager.py
@@ -21,6 +21,7 @@ events. It also provides a utility for waiting for slices to become active.
 from collections.abc import Callable, Mapping, Sequence, Set
 import functools
 import logging
+import threading
 from typing import Any, TypeVar
 
 import jax
@@ -32,6 +33,17 @@ _logger = logging.getLogger(__name__)
 
 class ElasticRuntimeError(RuntimeError):
   """Error raised when elasticity cannot continue."""
+
+
+class ScaleUpSignalError(Exception):
+  """Signals that the workload is ready to scale up.
+
+  This exception should be raised by user code when it detects that new hardware
+  is available and it wants to restart computation to make use of it.
+  Raising this exception will interrupt the current computation and cause the
+  elasticity manager to retry it with an updated slice configuration that
+  includes the new hardware.
+  """
 
 
 _F = TypeVar("_F", bound=Callable[..., Any])
@@ -54,11 +66,21 @@ def _elastic_event_cleanup() -> None:
 
 
 class Manager:
-  """Utility class for elastic training."""
+  """Utility class for elastic training.
 
-  _total_slice_count: int | None = None
+  Attributes:
+    slice_to_devices: A mapping from slice index to a sequence of `jax.Device`
+      objects for that slice.
+    all_slice_indices: A set of all possible slice indices.
+    active_slice_indices: A set of indices of the currently active slices.
+    new_slice_event: A `threading.Event` that is set when new slices become
+      available during replica/resize mode.
+  """
+
   slice_to_devices: Mapping[int, Sequence[jax.Device]]
+  all_slice_indices: Set[int]
   active_slice_indices: Set[int]
+  new_slice_event: threading.Event
 
   def __init__(self, devices: Sequence[jax.Device] | None = None) -> None:
     """Initializes the manager.
@@ -70,20 +92,21 @@ class Manager:
       devices = jax.devices()
     self.slice_to_devices = elastic.get_slice_to_devices(devices)
 
+    self.all_slice_indices = set(self.slice_to_devices.keys())
+
     self.active_slice_indices = elastic.get_active_slice_indices(
         slice_to_devices=self.slice_to_devices
     )
+    self.new_slice_event = threading.Event()
 
-  @property
+  @functools.cached_property
   def total_slice_count(self) -> int:
-    """Returns the total number of slices."""
-    if self._total_slice_count is None:
-      self._total_slice_count = len(self.slice_to_devices)
-    return self._total_slice_count
+    """The total number of slices."""
+    return len(self.slice_to_devices)
 
   @property
   def default_device(self) -> jax.Device:
-    """Returns the device that should be set to the default device.
+    """The device that should be set to the default device.
 
     This will be from one of the slices in `active_slice_indices`.
     """
@@ -94,8 +117,13 @@ class Manager:
 
   @property
   def active_slice_count(self) -> int:
-    """Returns the number of slices."""
+    """The number of active slices."""
     return len(self.active_slice_indices)
+
+  @property
+  def inactive_slice_indices(self) -> set[int]:
+    """The set of inactive slice indices."""
+    return self.all_slice_indices - self.active_slice_indices
 
   def scale_by_active_slices(self, x: int | float) -> int | float:
     """Scale x by the number of active slices."""
@@ -113,6 +141,20 @@ class Manager:
       return x * self.active_slice_count / self.total_slice_count
     else:
       raise ValueError(f"Unsupported type: {type(x)=}")
+
+  def _cleanup_on_retry(self):
+    """Cleans up JAX caches and traces on retry."""
+    try:
+      _logger.debug("Cleaning up any ongoing traces")
+      jax.profiler.stop_trace()
+    except (RuntimeError, ValueError):
+      _logger.debug("No ongoing traces to clean up")
+    except Exception:  # pylint: disable=broad-exception-caught
+      _logger.exception("Error cleaning up ongoing traces")
+
+    jax.clear_caches()
+    for array in jax.live_arrays():
+      array.delete()
 
   def _elasticity_retry_decorator(
       self,
@@ -148,9 +190,22 @@ class Manager:
 
             with jax.default_device(self.default_device):
               return func(*args, **kwargs)
+          except ScaleUpSignalError:
+            _logger.info("Scale up requested. Retrying.")
+            _elastic_event_cleanup()
+
+            if on_elastic_event_callback is not None:
+              on_elastic_event_callback()
           except jax.errors.JaxRuntimeError as error:
             if not elastic.is_error_due_to_slice_down(error):
               raise
+
+            if self.new_slice_event.is_set():
+              _logger.info(
+                  "Slice down event and new slice available detected. Retrying."
+              )
+            else:
+              _logger.info("Slice down event detected. Retrying.")
 
             _elastic_event_cleanup()
 
@@ -162,6 +217,7 @@ class Manager:
           )
 
       return wrapper
+
     return decorator
 
   def pause_resume(
@@ -197,7 +253,7 @@ class Manager:
         occurs.
 
     Returns:
-      The result of the wrapped function.
+      A decorator that retries the wrapped function.
 
     Raises:
       ElasticRuntimeError: If all retry attempts fail.
@@ -219,3 +275,100 @@ class Manager:
         pre_callback=internal_pre_callback,
         on_elastic_event_callback=on_elastic_event_callback,
     )
+
+  def _monitor_new_slices(
+      self, stop_event: threading.Event, poll_interval: float | int
+  ):
+    """Monitors for new slices and sets the `new_slice_event` if found."""
+    while not stop_event.wait(poll_interval):
+      try:
+        if not self.inactive_slice_indices:
+          _logger.debug("No inactive slices to check.")
+          continue
+
+        _logger.debug(
+            "Checking inactive slices: %s", self.inactive_slice_indices
+        )
+        inactive_slice_to_devices = {
+            i: self.slice_to_devices[i] for i in self.inactive_slice_indices
+        }
+        newly_active_indices = elastic.get_active_slice_indices(
+            inactive_slice_to_devices
+        )
+
+        if newly_active_indices:
+          _logger.info(
+              "New slices found: %s. Setting new slice event.",
+              newly_active_indices,
+          )
+          self.new_slice_event.set()
+          return
+
+        _logger.debug("No new slices found.")
+      except Exception:  # pylint: disable=broad-exception-caught
+        _logger.exception("Error in monitor thread")
+
+  def replica_resize(
+      self,
+      max_resizes: int,
+      poll_interval: float = 10,
+      pre_callback: Callable[..., Any] | None = None,
+      on_elastic_event_callback: Callable[..., Any] | None = None,
+  ) -> Callable[[_F], _F]:
+    """Retries a function with replica/resize fault tolerance.
+
+    Args:
+      max_resizes: The maximum number of times to retry the function after
+        resizing the replica count.
+      poll_interval: The number of seconds to wait between active slice checks.
+        Defaults to 10 seconds.
+      pre_callback: A callback to call before the function is attempted.
+      on_elastic_event_callback: A callback to call after an elastic failure
+        occurs.
+
+    Returns:
+      A decorator that retries the wrapped function.
+
+    Raises:
+      ElasticRuntimeError: If all retry attempts fail.
+      Exception: Any other exception raised by the wrapped function that is not
+        due to a slice down event.
+    """
+
+    def internal_pre_callback():
+      self.active_slice_indices = elastic.wait_for_slices(
+          slice_count=1,
+          slice_to_devices=self.slice_to_devices,
+          poll_interval=poll_interval,
+      )
+
+      if pre_callback is not None:
+        pre_callback()
+
+    retry_decorator = self._elasticity_retry_decorator(
+        max_retries=max_resizes,
+        pre_callback=internal_pre_callback,
+        on_elastic_event_callback=on_elastic_event_callback,
+    )
+
+    def decorator(func):
+      @functools.wraps(func)
+      def wrapper(*args, **kwargs):
+        self.new_slice_event.clear()
+        stop_event = threading.Event()
+
+        monitor_thread = threading.Thread(
+            target=self._monitor_new_slices,
+            args=(stop_event, poll_interval),
+            daemon=True,
+        )
+        monitor_thread.start()
+        try:
+          return func(*args, **kwargs)
+        finally:
+          stop_event.set()
+          monitor_thread.join()
+
+      return retry_decorator(wrapper)
+
+    return decorator
